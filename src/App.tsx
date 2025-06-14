f1c1b08edfa123dfe666febbae954f9e00453468
aleea <alyamachmud2005@gmail.com>
2025-06-15 00:04:19 +0500

    Add my custom banner

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,28 @@ import Experience from "./components/Experience";
 import { Suspense } from "react";
 import * as THREE from "three";
 import { Perf } from "r3f-perf";
+import { Leva } from "leva";
 
 const App = () => {
   return (
-    <Canvas
-      shadows={{ type: THREE.PCFSoftShadowMap }}
-      camera={{ position: [0, 8, 15], near: 0.1, fov: 60, far: 200 }}
-      gl={{
-        antialias: true,
-        toneMapping: THREE.ACESFilmicToneMapping,
-        toneMappingExposure: 1.0,
-      }}
-    >
-      <Perf />
-      <Suspense fallback={null}>
-        <Experience />
-      </Suspense>
-    </Canvas>
+    <>
+      <Leva collapsed />
+
+      <Canvas
+        shadows={{ type: THREE.PCFSoftShadowMap }}
+        camera={{ position: [0, 8, 15], near: 0.1, fov: 60, far: 200 }}
+        gl={{
+          antialias: true,
+          toneMapping: THREE.ACESFilmicToneMapping,
+          toneMappingExposure: 1.0,
+        }}
+      >
+        {/* <Perf /> */}
+        <Suspense fallback={null}>
+          <Experience />
+        </Suspense>
+      </Canvas>
+    </>
   );
 };
 
